e15a3b4f78c5c6416214a4bdde76637a7284ec2f
Jerome Swannack <jeromeswannack@gmail.com>
2022-02-19 09:17:03 +1300

    added app image launcher and docker

--- linux/install.sh
+++ linux/install.sh
@@ -50,3 +50,22 @@ sudo apt install -y brave-browser
 # nodejs
 sudo apt-get install -y nodejs
 sudo apt install -y npm
+
+# docker
+sudo apt-get update
+sudo apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# App Image Launcher
+sudo add-apt-repository -y ppa:appimagelauncher-team/stable
+sudo apt-get update
+sudo apt-get install -y appimagelauncher
