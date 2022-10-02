04a07f1e343804866f9b72f7d97117696554c277
Jerome Swannack <jeromeswannack@gmail.com>
2022-10-03 09:12:43 +1300

    removed python alias, added lenv

--- linux/config/.jeromebashrc.sh
+++ linux/config/.jeromebashrc.sh
@@ -126,6 +126,7 @@ fi
 
 alias gs='git status'
 alias hk='~/.hotkeys_on.sh'
+alias lenv='export $(cat <repo path>/.env | xargs)'
 
 # git log
 function gl(){
@@ -222,7 +223,6 @@ function getcommitprefix(){
 	fi
 }
 
-alias python=python3
 alias serve="browser-sync start -s -f . --no-notify --host 0.0.0.0 --port 9000"
 
 function gg() {
