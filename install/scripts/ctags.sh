9e79f8eef337cb70cfb9cc53a0411936f02dd2a1
Jerome Swannack <jeromeswannack@gmail.com>
2022-10-11 21:54:14 +1300

    Using sudo for make install of ctags

--- install/scripts/ctags.sh
+++ install/scripts/ctags.sh
@@ -4,5 +4,5 @@ cd ~/source/ctags
 ./autogen.sh
 ./configure --prefix=/where/you/want # defaults to /usr/local
 make
-make install # may require extra privileges depending on where to install
+sudo make install # may require extra privileges depending on where to install
 cd -
