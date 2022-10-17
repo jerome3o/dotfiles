2224484edbf29ef28fafafa748c4812f8a18b8ff
Jerome Swannack <jeromeswannack@gmail.com>
2022-10-17 22:21:46 +1300

    Added bind for NERDTree

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,12 @@
 require("plugins")
 -- require("lspkeybinds")
 
+local function map(m, k, v)
+  vim.keymap.set(m, k, v, {silent = true})
+end
+
+map('n', '-', ':NERDTree<CR>')
+
 vim.cmd([[
   set number relativenumber
 ]])
