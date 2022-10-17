aae1c00a7b480ec62eb583f6ac066dc8aac50380
Jerome Swannack <jeromeswannack@gmail.com>
2022-10-18 12:10:14 +1300

    Added tmux entry point to bashrc

--- bash/.jeromebashrc.sh
+++ bash/.jeromebashrc.sh
@@ -206,3 +206,8 @@ function getcommitprefix(){
 	fi
 }
 
+## Tmux Entry
+
+if [ -x "$(command -v tmux)" ] && [ -n "${DISPLAY}" ] && [ -z "${TMUX}" ]; then
+    tmux attach || tmux >/dev/null 2>&1
+fi
