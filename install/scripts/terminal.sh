d5d72e1086ea93285248a29eadcea2c73ccd537e
Jerome Swannack <jeromeswannack@gmail.com>
2022-10-20 15:17:43 +1300

    added ranger to terminal.sh

--- install/scripts/terminal.sh
+++ install/scripts/terminal.sh
@@ -6,6 +6,8 @@ if [ -f ~/.jeromebashrc.sh ]; then
 fi
 EOF
 
+sudo apt install ranger
+
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 
 . ~/scripts/nvim.sh
