af1029633dff11ba9bbc58d7e3a2dd29219e2d6a
Jerome Swannack <jeromeswannack@gmail.com>
2022-10-20 16:41:03 +1300

    added compose to docker script

--- install/scripts/docker.sh
+++ install/scripts/docker.sh
@@ -16,3 +16,7 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+curl -SL https://github.com/docker/compose/releases/download/v2.12.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
