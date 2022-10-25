a47f955e8d30a50cdc7de3fff5d3db8af590e91c
Jerome Swannack <jeromeswannack@gmail.com>
2022-10-25 15:52:36 +1300

    commented out tmux entry

--- bash/.jeromebashrc.sh
+++ bash/.jeromebashrc.sh
@@ -218,6 +218,6 @@ function getcommitprefix(){
 
 ## Tmux Entry
 
-if [ -x "$(command -v tmux)" ] && [ -n "${DISPLAY}" ] && [ -z "${TMUX}" ]; then
-    tmux attach || tmux >/dev/null 2>&1
-fi
+# if [ -x "$(command -v tmux)" ] && [ -n "${DISPLAY}" ] && [ -z "${TMUX}" ]; then
+#     tmux attach || tmux >/dev/null 2>&1
+# fi
