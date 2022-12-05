34dd4394a7c7bf058d0a1c5505c77024a9084e95
Jerome Swannack <jeromeswannack@gmail.com>
2022-12-05 13:33:59 +1300

    Configured gitsigns

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -26,7 +26,12 @@ return require('packer').startup(function(use)
   use "preservim/tagbar"
   use "ryanoasis/vim-devicons"
   use "tpope/vim-commentary"
-  use "lewis6991/gitsigns.nvim"
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
   -- use "neovim/nvim-lspconfig"
   use "mg979/vim-visual-multi"
   use {
