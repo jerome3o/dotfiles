e3851c332503dd8d0036c3be1a114904ae8f1aa4
Jerome Swannack <jeromeswannack@gmail.com>
2022-12-11 16:51:15 +1300

    Adjusting fonts.sh for arch

--- install/scripts/fonts.sh
+++ install/scripts/fonts.sh
@@ -1,4 +1,8 @@
 mkdir -p ~/.fonts/
+mkdir -p ~/.local/share/fonts/
 wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Ubuntu/Medium/complete/Ubuntu%20Medium%20Nerd%20Font%20Complete.ttf
-mv Ubuntu\ Medium\ Nerd\ Font\ Complete.ttf ~/.fonts/
+cp Ubuntu\ Medium\ Nerd\ Font\ Complete.ttf ~/.fonts/
+cp Ubuntu\ Medium\ Nerd\ Font\ Complete.ttf ~/.local/share/fonts/
+rm Ubuntu\ Medium\ Nerd\ Font\ Complete.ttf 
+
 fc-cache -f -v
