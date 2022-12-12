156b2a9df068f0e73290bc5aa0b68a0c95442f9f
Jerome Swannack <jeromeswannack@gmail.com>
2022-12-12 21:16:04 +1300

    Added c to nvim treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "python", "lua", "vim", "json" },
+  ensure_installed = { "python", "lua", "vim", "json", "c" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
