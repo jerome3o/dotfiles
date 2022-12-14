59e703916689a1a8efe049f4c6d2d01fac3fc2e1
Jerome Swannack <jeromeswannack@gmail.com>
2022-12-14 17:10:43 +1300

    Tinkering with snippet

--- nvim/.config/nvim/lua/core/packer.lua
+++ nvim/.config/nvim/lua/core/packer.lua
@@ -84,7 +84,7 @@ return require('packer').startup(function(use)
 
       -- Snippets
       { 'L3MON4D3/LuaSnip' },
-      { 'rafamadriz/friendly-snippets' },
+      -- { 'rafamadriz/friendly-snippets' },
     }
   }
 
--- nvim/.config/nvim/lua/plugins/luasnips_config.lua
+++ nvim/.config/nvim/lua/plugins/luasnips_config.lua
@@ -1,10 +1,11 @@
--- require("luasnip.loaders.from_vscode").load({
---   paths = { "~/.config/Code/User/snippets" },
--- })
+require("luasnip.loaders.from_vscode").load({
+  paths = { "~/dotfiles/vscode/.config/Code/User/snippets" },
+})
 
 -- local ls = require("luasnip")
 -- ls.filetype_extend("all", {"python"})
 
-local ls = require("luasnip")
+-- local ls = require("luasnip")
 
-require("luasnip.loaders.from_vscode").load({ paths = "~/.config/nvim/snippets/" })
+-- require("luasnip.loaders.from_vscode").load({ paths = "~/.config/nvim/snippets/" })
+-- require("luasnip.loaders.from_vscode").load({ paths = "~/tmp/friendly-snippets/" })
