eb8412a3435ac15b12804eeae2f0943af34aee54
Jerome Swannack <jeromeswannack@gmail.com>
2022-12-23 16:16:15 +1300

    Added vscode load_launchjs, added terminate map

--- nvim/.config/nvim/lua/plugins/dap_config.lua
+++ nvim/.config/nvim/lua/plugins/dap_config.lua
@@ -13,22 +13,23 @@ local dap = require('dap')
 -- }
 
 require("dap-python").setup()
+require('dap.ext.vscode').load_launchjs()
 
 -- keymaps
 
 local map = function(m, lhs, rhs)
-  local opts = {remap = false, silent = false}
+  local opts = { remap = false, silent = false }
   vim.keymap.set(m, lhs, rhs, opts)
 end
 
 vim.fn.sign_define(
   'DapBreakpoint',
-  {text='🛑', texthl='', linehl='', numhl=''}
+  { text = '🛑', texthl = '', linehl = '', numhl = '' }
 )
 
+map('n', '<S-F5>', ':lua require"dap".terminate()<CR>')
 map('n', '<F5>', ':lua require"dap".continue()<CR>')
 map('n', '<F9>', ':lua require"dap".toggle_breakpoint()<CR>')
 map('n', '<F10>', ':lua require"dap".step_over()<CR>')
 map('n', '<F11>', ':lua require"dap".step_into()<CR>')
 map('n', '<leader>y', ':lua require"dap".repl.open()<CR>')
-
