cdd9cd2acd6b9ab2823252450485da62900df853
Jerome Swannack <jeromeswannack@gmail.com>
2022-12-24 11:58:18 +1300

    Fixing null ls config

--- nvim/.config/nvim/lua/core/packer.lua
+++ nvim/.config/nvim/lua/core/packer.lua
@@ -102,7 +102,7 @@ return require('packer').startup(function(use)
   use { "jose-elias-alvarez/null-ls.nvim",
     requires = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("_null-ls")
+      require("null-ls").setup()
     end
   }
 
