e2b0691e94868041d05155e5085c7ca82d980362
Jerome Swannack <jeromeswannack@gmail.com>
2022-12-28 00:31:54 +1300

    Preventing blamer from showing up in visual mode

--- nvim/.config/nvim/lua/plugins/blamer.lua
+++ nvim/.config/nvim/lua/plugins/blamer.lua
@@ -2,3 +2,11 @@ vim.g.blamer_enabled = 1
 vim.g.blamer_delay = 300
 vim.g.blamer_prefix = " -- "
 vim.g.blamer_show_in_insert_modes = 0
+vim.g.blamer_show_in_visual_modes = 0
+
+
+-- Function that saves the current cursor positions
+function save_cursor()
+  vim.cmd("let g:cursor_line = line('.')")
+  vim.cmd("let g:cursor_col = col('.')")
+end
