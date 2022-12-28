651433e043b1b0e58d3d34fad8acc3b27e2930de
Jerome Swannack <jeromeswannack@gmail.com>
2022-12-28 19:53:14 +1300

    Removed random snippet from blamer config

--- nvim/.config/nvim/lua/plugins/blamer.lua
+++ nvim/.config/nvim/lua/plugins/blamer.lua
@@ -3,10 +3,3 @@ vim.g.blamer_delay = 300
 vim.g.blamer_prefix = " -- "
 vim.g.blamer_show_in_insert_modes = 0
 vim.g.blamer_show_in_visual_modes = 0
-
-
--- Function that saves the current cursor positions
-function save_cursor()
-  vim.cmd("let g:cursor_line = line('.')")
-  vim.cmd("let g:cursor_col = col('.')")
-end
