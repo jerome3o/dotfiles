07947c0f49de360559f46ed9cbae70edecb88b98
Jerome Swannack <jeromeswannack@gmail.com>
2023-01-19 14:37:36 +1300

    Not formatting momentum

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -1,7 +1,11 @@
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   callback = function()
-    if vim.lsp.buf.server_ready() then
-      vim.lsp.buf.format()
+    local current_dir = debug.getinfo(1).source:match("@(.*/)")
+    if string.find(current_dir, "momentum") then
+    else
+      if vim.lsp.buf.server_ready() then
+        vim.lsp.buf.format()
+      end
     end
   end,
 })
