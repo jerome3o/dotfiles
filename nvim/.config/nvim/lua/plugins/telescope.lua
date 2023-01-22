57282d395613ec84bc5da14b1c20f00e215296f3
Jerome Swannack <jeromeswannack@gmail.com>
2023-01-23 11:30:26 +1300

    Added unrestricted finders

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -13,6 +13,10 @@ require('telescope').setup {
   }
 }
 
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require('telescope').load_extension('fzf')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
@@ -22,6 +26,27 @@ vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>.', builtin.treesitter, {})
 
--- To get fzf loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
-require('telescope').load_extension('fzf')
+local function live_grep_all()
+  builtin.live_grep {
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '-u'
+    }
+  }
+end
+
+local function find_files_all()
+  builtin.find_files {
+    hidden = true,
+    no_ignore = true,
+  }
+end
+
+vim.keymap.set('n', '<leader>fA', live_grep_all, {})
+vim.keymap.set('n', '<leader>fa', find_files_all, {})
