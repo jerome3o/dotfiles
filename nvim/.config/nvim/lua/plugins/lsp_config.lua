005b1e098e9687acc0078427200916dc20a82bcc
Jerome Swannack <jeromeswannack@gmail.com>
2023-02-09 22:48:52 +1300

    Ruff off, updated format command

--- nvim/.config/nvim/lua/plugins/lsp_config.lua
+++ nvim/.config/nvim/lua/plugins/lsp_config.lua
@@ -43,7 +43,7 @@ local on_attach = function(client, bufnr)
     vim.api.nvim_create_autocmd("BufWritePre", {
       group = vim.api.nvim_create_augroup("Format", { clear = true }),
       buffer = bufnr,
-      callback = function() vim.lsp.buf.formatting_seq_sync() end
+      callback = function() vim.lsp.buf.format() end
     })
   end
 end
@@ -74,6 +74,6 @@ require("lspconfig").sumneko_lua.setup({
 })
 
 -- Add ruff
-require('lspconfig').ruff_lsp.setup {
-  on_attach = on_attach,
-}
+-- require('lspconfig').ruff_lsp.setup {
+--   on_attach = on_attach,
+-- }
