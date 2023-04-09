a1e90cab867eccfbb707dec33fa21e309ff7848e
Jerome Swannack <jeromeswannack@gmail.com>
2023-04-10 09:36:50 +1200

    Hibernating on lid closed

--- i3/.config/i3/lock.sh
+++ i3/.config/i3/lock.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-set -e
-xset s off dpms 0 10 0
+# set -e
+# xset s off dpms 0 10 0
+# xset s off -dpms
+systemctl hibernate
 i3lock --color=000000 --ignore-empty-password --show-failed-attempts --nofork
-xset s off -dpms
