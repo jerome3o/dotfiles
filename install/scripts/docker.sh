017fa92f466ad3fc7a2cef34ad3de422266527e7
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-10 01:57:21 +1200

    Added -y's to docker script

--- install/scripts/docker.sh
+++ install/scripts/docker.sh
@@ -2,7 +2,7 @@ set -ex
 
 sudo apt-get update
 
-sudo apt-get install \
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -17,7 +17,7 @@ echo \
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 curl -SL https://github.com/docker/compose/releases/download/v2.12.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 
